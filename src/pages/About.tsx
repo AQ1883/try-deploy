4f21a6e085cca4d43cd62917978b198c2f47e07b
aq1883 <amq1430@gmail.com>
2024-11-06 21:43:36 +0300

    backup

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,16 +1,22 @@
-import { div } from 'framer-motion/client'
-import React from 'react'
-import Abouts1 from '../Componts/About-s1'
+import Abouts1 from "../Componts/About-s1";
+import Abouts2 from "../Componts/abouts2";
+import Abouts3 from "../Componts/abouts3";
+import Abouts4 from "../Componts/abouts4";
 
 function About() {
   return (
     <>
-    <div className='h-screen'>
-      <Abouts1/>
-    </div>
-    
+      <div className="h-screen">
+        <Abouts1 />
+      </div>
+      <div className="bg-black ">
+        <Abouts2 />
+        <Abouts3 />
+
+        <Abouts4 />
+      </div>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
