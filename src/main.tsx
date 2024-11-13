3711e21bb89b9474ad6c3deb80e5585aef5644af
aq1883 <amq1430@gmail.com>
2024-11-13 20:19:55 +0300

    fix all problems

--- src/main.tsx
+++ src/main.tsx
@@ -3,34 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "@material-tailwind/react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
 import Services from "./pages/Services.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/About",
-    element: <About />,
-  },
-  {
-    path: "/Contact",
-    element: <Contact />,
-  },
-  {
-    path: "/Services",
-    element: <Services />,
-  },
-]);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Services" element={<Services />} />
+        </Routes>
+      </HashRouter>
     </ThemeProvider>
   </StrictMode>
 );
