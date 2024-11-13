3e0a2e30b5ed4e77fe5ec8e36673dd332b067625
aq1883 <amq1430@gmail.com>
2024-11-13 20:44:45 +0300

    fix all problems

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/try-deploy/',  // يجب تحديد المسار الأساسي
   build: {
     outDir: 'build',  // تأكد أن هذا هو المجلد الذي تستخدمه في gh-pages
   },
-})
+});
